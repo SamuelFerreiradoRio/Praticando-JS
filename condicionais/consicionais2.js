34c8177d43faafdde6d9676b77cbb9f7262e75a2
SamuelFerreiradoRio <samucafr53@gmail.com>
2026-03-18 17:35:25 -0300

    :construction: feat Ralização dos exrcícios de estrutura condicional com js.

---
+++ condicionais/consicionais2.js
@@ -0,0 +1,49 @@
+const idade = 17;
+
+if (idade < 18) {
+  console.log("Menor de idade");
+} else {
+  console.log("Maior de idade");
+}
+//______________________________________________________
+let numero = Number(prompt("Digite um numero"));
+if(numero > 0) {
+   console.log("Positivo");
+}
+else if(numero < 0) {
+   console.log("Negativo")
+}
+
+//______________________________________________________
+const nota = 80;
+
+if (nota > 60) {
+    console.log("Aprovado!!!");
+  }
+  
+else if (nota < 60) {
+  console.log("Reprovado.");
+    }
+//______________________________________________________
+let Numero = Number(prompt("Verificação do seu numero: Positivo, Negativo ou Zero"));
+if(Numero > 0) {
+   console.log("Positivo");
+}
+else if(Numero < 0) {
+   console.log("Negativo")
+}
+else if(Nmero = 0) {
+   console.log("Zero")
+}
+//______________________________________________________
+const Idade = (prompt("Verificação de Idade"));
+if(Numero > 0 && 12) {
+   console.log("Criança");
+}
+else if(Numero < 13 && 17) {
+   console.log("Adolecente")
+}
+else if(Nmero > 18) {
+   console.log("Adulto")
+}
+//________________________________________________________
